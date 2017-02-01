8d36977591925d0c8fd1c1fc38a7ec0fe713cd3a
Noah Panicola <noahpanicola@gmail.com>
2017-01-31 21:48:44 -0600

    stuff

--- LazyEggsTest/ViewController.swift
+++ LazyEggsTest/ViewController.swift
@@ -12,71 +12,16 @@ import Firebase
 import FirebaseDatabase
 import GoogleSignIn
 
-class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+class ViewController: UIViewController, GIDSignInUIDelegate {
     
     var databaseRef: FIRDatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.checkUserStatus()
         // Do any additional setup after loading the view, typically from a nib.
-        GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
-        GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().uiDelegate = self
     }
-    
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        return GIDSignIn.sharedInstance().handle(url,
-                                                 sourceApplication: sourceApplication,
-                                                 annotation: annotation)
-    }
-    
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
-        
-        if let error = error {
-            print("error: " + error.localizedDescription)
-            return
-        }
-        print("User signed into Google\n")
-        
-        guard let authentication = user.authentication else { return }
-        let credential = FIRGoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
-        
-        FIRAuth.auth()?.signIn(with: credential) { (user, error) in
-            
-            if let error = error {
-                print("error1234: " + error.localizedDescription)
-                return
-            }
-            
-            print("User signed into Firebase")
-            self.databaseRef = FIRDatabase.database().reference()
-            self.databaseRef.child("user_profiles").child(user!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
-                let snapshot = snapshot.value as? NSDictionary
-                if snapshot == nil{
-                    self.databaseRef.child("user_profiles").child("name").setValue(user?.displayName)
-                    self.databaseRef.child("user_profiles").child("email").setValue(user?.email)
-                }
-                
-                
-            })
-            
-            self.performSegue(withIdentifier: Constants.Segues.SignInToHome, sender: nil)
-            
-            
-        }
-    }
-    
-    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
-        
-    }
-    
-    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
-        -> Bool {
-            return GIDSignIn.sharedInstance().handle(url,
-                                                     sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
-                                                     annotation: [:])
-    }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -86,8 +31,17 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     @IBAction func signInWithGoogle(_ sender: LineButton) {
         GIDSignIn.sharedInstance().signIn()
     }
-
-
+    
+    func checkUserStatus(){
+        if FIRAuth.auth()?.currentUser != nil {
+            presentHome()
+        } else {
+            //User Not logged in
+        }
+    }
+    func presentHome(){
+        
+    }
 
 }
 
