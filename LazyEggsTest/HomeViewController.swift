500b68cd5e5331f94e98d84bd5522243d9ea8b0e
Tony Forsythe <forsythetony@gmail.com>
2017-02-02 19:36:20 -0600

    "forgot something"

--- LazyEggsTest/HomeViewController.swift
+++ LazyEggsTest/HomeViewController.swift
@@ -13,7 +13,6 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mainTextView.text = ""
         
         // Do any additional setup after loading the view.
     }
